97d08787b6bf89ac5c647b8f40711e967a163135
ortizjs <jonnatano94@gmail.com>
2018-12-07 16:23:21 -0800

    work on session_controller

--- BenchBnB/app/controllers/api/sessions_controller.rb
+++ BenchBnB/app/controllers/api/sessions_controller.rb
@@ -3,9 +3,18 @@ class Api::SessionsController < ApplicationController
 
 
   def create
-    
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      flash.now[:errors] = ["Invalid credentials"]
+    else
+      login(@user)
+      # redirect_to
+    end
   end
 
   def destroy
+    logout(@user)
+    redirect_to new_session_url
   end
+
 end
